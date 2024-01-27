5d38a8452a9227e89171c3045fea0568d11f5916
yokishi <yk.0402.4032@gmail.com>
2024-01-27 15:38:29 +0900

    2024/01/27:デプロイ先変更

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: (process.env.NODE_ENV === 'poduction')
+    ? '/hoshidora-hero/' : './',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     vue(),
   ],
